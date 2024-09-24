c23ecff3aa4c8ae8d9c78b455d4676a9f5a525f0
Surya Prakash Rakkasi <2000129936@hexaware.com>
2024-09-24 17:16:00 +0000

    developing stg_orders

---
+++ models/staging/stg_orders.sql
@@ -0,0 +1,18 @@
+select o.ORDERID,
+o.ORDERDATE,
+o.SHIPDATE,
+o.SHIPMODE,
+o.ordersellingprice-o.ordercostprice as orderprofit,
+o.ordercostprice,
+o.ordersellingprice,
+c.customername,
+c.segment,
+c.country,
+p.category,
+p.productname,
+p.subcategory
+from {{ ref('raw_orders') }} as o
+left join {{ ref('raw_customers') }} as c
+on o.customerid=c.customerid
+left join {{ ref('raw_products') }} as p
+on o.productid=p.productid
